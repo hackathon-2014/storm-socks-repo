ac8237e70b4d4a9edf726ad668763df56b1c1367
brentcobb <brent.tm.cobb@gmail.com>
2014-08-23 18:54:49 -0400

    i added a fancy button that looks like an a tag but its actually an a tag that looks like a button

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -6,5 +6,9 @@ angular.module('gepettoApp')
       .when('/', {
         templateUrl: 'app/main/main.html',
         controller: 'MainCtrl'
+      })
+      .when('/build', {
+        templateUrl: 'app/build/build.html',
+        controller: 'BuildCtrl'
       });
   });
