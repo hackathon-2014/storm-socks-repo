24d2ef44fdcbfa17689ccf0751f06d750398fdbc
brentcobb <brent.tm.cobb@gmail.com>
2014-08-23 12:10:29 -0400

    such push

--- app/app.js
+++ app/app.js
@@ -1,9 +1,7 @@
-require('angular/angular');
-require('angular-route/angular-route');
-
+require('angular');
 
 angular.module('Gepetto', ['ui.bootstrap', 'ngRoute']).config(['$routeProvider', function($routeProvider) {
-
+  console.log("yololo:");
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html'});
   $routeProvider.otherwise({redirectTo: '/view1'});
