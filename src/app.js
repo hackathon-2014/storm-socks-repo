60558a02b462dc08c857204fa8dfeae8611547a7
Chazu <chaz@plaidpotion.com>
2014-08-23 11:02:59 -0400

    initial commit

---
+++ src/app.js
@@ -0,0 +1,44 @@
+// Node modules
+var sys = require('sys');
+var http = require('http');
+var fs = require('fs');
+
+// Express modules
+var express = require('express');
+var session = require('cookie-session');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var handlebars = require('handlebars');
+var logger = require('morgan');
+
+// Misc 3rd party modules
+var colors = require('colors');
+var moment = require('moment');
+
+// Initialize express and OSC client
+var app = express();
+var router = express.Router();
+
+// Set up environment-specific express data
+var serverRoot = __dirname;
+
+// Configure express app
+
+//.use(logger())
+  app.use(bodyParser())
+  // .use(session({
+  //   secret: "secret",
+  //   cookie: {"gepetto": {}}
+  // }));
+app.use('/js', express.static(serverRoot + "/js"));
+app.use('/css', express.static(serverRoot + "/css"));
+app.use('/img', express.static(serverRoot + "/img"));
+app.use("/", router);
+
+//// Serve assets & index page
+router.get('/', function(req, res) {
+});
+
+
+
+http.createServer(app).listen(3000);
