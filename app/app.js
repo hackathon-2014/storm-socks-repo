ccd009548f49bbe18e1ef7274ea33c65a308c7f9
Chazu <chaz@plaidpotion.com>
2014-08-23 16:18:46 -0400

    Z̜̗̻̃̐̂̀̕͠A͉͍̱͇̞̗̼ͬ̓̏ͫL̷̮̦̞̦̳͍̽̅̃͘G͔̤̤̖͆̏ͫ̈́͛͘͡O̤̘̥̞͖̲̟̼͆̎ͭ͢͜͢ ͔̳̀̇ͨ̔͗̚͟͜͜C̶̩̳̖͐ͭͪ̍̑͐̎͜ͅO͉̝͑͌ͫ̓̑̂́̓M̧̹̖̲͎͍̾ͬͩ̀E̘͕͔͙͉̩̪͉ͫ̓ͮ͐͋̔S̴̝̟͕̤͓̞̙͚͑ͯ̑ͥ̏ ̛̪͙͕̭ͮ̊̋͐ͫ̃ͪ̀Í̤̭̰͙͒̉͛̇ͧN̳̭̑ͩ̅̃̃̊̃ ̨̗͎̖͚̺̮͙̊̂͂̔̑ͦ͐T̬̞̬͔̣͋ͬH̬̙̲͖̘͒̑ͩ̿̕͞E̥ͩ̅̏͛ ̛̫̬̝̗͚ͯ̿͂͠N͑ͣ̃͗̔ͮ͏̛̪̜͓̭͘I̻̩̼͖̟̖͓͎̠ͥ̃͋̈̽͒G͐̏ͤͣͩͪ͆͏͙̤̜̱͖̀Hͤ̎̂͛͛̈͏̴̗̤̩̰ͅͅT̷̟͚̳̱͚͙ͪͥ̌̓͠

--- app/app.js
+++ app/app.js
@@ -5,13 +5,14 @@ var sceneViewer = require('./sceneViewer.js');
 require('angular');
 require('angular-bootstrap');
 require('angular-route');
+var mainctrl = require('./main');
 
   $('document').ready(function(){
     var scene = new sceneViewer("renderCanvas")
+    document.scene = scene;
     scene.run();
-    debugger;
-    console.log("addin a block here boss");
     scene.addBlock();
+    console.log("addin a block here boss");
   });
 
 angular.module('Gepetto', ['ui.bootstrap', 'ngRoute']).config(function($routeProvider) {
@@ -22,7 +23,4 @@ angular.module('Gepetto', ['ui.bootstrap', 'ngRoute']).config(function($routePro
     .when('/', {templateUrl: 'views/main.html', controller: 'MainCtrl'})
     .otherwise({templateUrl: '/views/404.html'});
   
-
 }).controller('MainCtrl', ['$scope', mainctrl.MainCtrl]);
-
-
