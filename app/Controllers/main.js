9cfa89fae06ab1ea2d62e37a770c610fac3af84c
brentcobb <brent.tm.cobb@gmail.com>
2014-08-23 14:42:58 -0400

    angular yoboi

--- app/Controllers/main.js
+++
@@ -1,4 +0,0 @@
-app.controller('MainCtrl', function($scope) {
-  console.log("loadedmainCtrl:");
-
-});
--- app/app.js
+++ app/app.js
@@ -1,13 +1,15 @@
 require('angular');
 require('angular-bootstrap');
 require('angular-route');
+var mainctrl = require('./main');
+
 console.log("this:");
-var app = angular.module('Gepetto', ['ui.bootstrap', 'ngRoute']).config(function($routeProvider) {
+angular.module('Gepetto', ['ui.bootstrap', 'ngRoute']).config(function($routeProvider) {
 
   $routeProvider
     .when('/', {templateUrl: 'views/main.html', controller: 'MainCtrl'})
     .otherwise({templateUrl: '/views/404.html'});
  
 
-});
+}).controller('MainCtrl', ['$scope', mainctrl.MainCtrl]);
 
